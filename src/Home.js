b264350042fb07174045b74ca23ff93bfd9695c4
Kaveri <846336@cognizant.com>
2020-02-18 15:25:48 +0530

    Commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Home extends Component {
+    render() {
+        return (
+            <div>
+              WelCome TO Home Component
+            </div>
+        )
+    }
+}
+
+export default  Home
